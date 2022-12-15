ddbac02fded273637d76219a7c96f74bb847a7c8
Fabio Cosentino <fabiocosentino7@gmail.com>
2022-12-15 11:59:35 +0100

    fix update for routes

--- routes/clientRoutes.py
+++ routes/clientRoutes.py
@@ -23,11 +23,3 @@ async def add_client(client : Client):
 async def update_client(client_id : str, new_data : Client):
     new_data_dict = new_data.dict()
     return Service.update_client(client_id, new_data_dict)
-
-@router.patch("/patch")
-async def patch_client(client_id : str, new_data : Client):
-    in_db_client = Service.get_client_by_id_service(client_id)
-    in_db_client_model = Client(**in_db_client.dict())
-    db_data_to_patch = new_data.dict(exclude_unset=True)
-    patched_db = in_db_client_model.copy(update=db_data_to_patch)
-    return Service.update_company(client_id, patched_db.dict())
--- routes/company_clientRoutes.py
+++ routes/company_clientRoutes.py
@@ -25,14 +25,8 @@ async def delete_company_client_by_id(filter : str, id : str):
 @router.put("/put")
 async def put_company(filter : str , id : str, new_data : Company_Client):
     if filter == "client":
-        in_db_comapny_client = Service.get_company_client_by_client_id_service(id)
-        in_db_comapny_client_model = Company_Client(**in_db_comapny_client())
-        db_data_to_patch = new_data.dict(exclude_unset=True)
-        patched_db = in_db_comapny_client_model.copy(update=db_data_to_patch)
-        return Service.update_company_client_by_client_id(id, patched_db.dict())
+        new_data_dict = new_data.dict()
+        return Service.update_company_client_by_client_id(id, new_data_dict)
     elif filter == "company":
-        in_db_comapny_client = Service.get_company_client_by_company_id_service(id)
-        in_db_comapny_client_model = Company_Client(**in_db_comapny_client())
-        db_data_to_patch = new_data.dict(exclude_unset=True)
-        patched_db = in_db_comapny_client_model.copy(update=db_data_to_patch)
-        return Service.update_company_client_by_company_id(id, patched_db.dict())
+        new_data_dict = new_data.dict()
+        return Service.update_company_client_by_company_id(id, new_data_dict)
