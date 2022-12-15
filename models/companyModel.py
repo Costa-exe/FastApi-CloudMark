665dc19669b851ca3caf312013b8fe62e890650d
Fabio Cosentino <fabiocosentino7@gmail.com>
2022-12-15 15:06:06 +0100

    added validators to models

--- models/companyModel.py
+++ models/companyModel.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 class Company(BaseModel):
 
@@ -12,3 +12,63 @@ class Company(BaseModel):
     email : str
     pec : str
     fax: str
+
+    @validator('id_azienda')
+    def validate_id(cls, v):
+        assert len(v) <= 80, f'maximum length for {v} is 80 characters.'
+        assert len(v) > 0, f'{v} must contain at least 1 character.'
+        return v
+
+    @validator('nome')
+    def validate_id(cls, v):
+        assert len(v) <= 90, f'maximum length for {v} is 90 characters.'
+        assert len(v) > 0, f'{v} must contain at least 1 character.'
+        return v
+
+    @validator('p_iva')
+    def validate_id(cls, v):
+        assert len(v) <= 11, f'maximum length for {v} is 11 characters.'
+        assert len(v) > 0, f'{v} must contain at least 1 character.'
+        return v
+
+    @validator('indirizzo')
+    def validate_id(cls, v):
+        assert len(v) <= 90, f'maximum length for {v} is 90 characters.'
+        assert len(v) > 0, f'{v} must contain at least 1 character.'
+        return v
+
+    @validator('cap')
+    def validate_id(cls, v):
+        assert len(v) <= 5, f'maximum length for {v} is 5 characters.'
+        assert len(v) > 0, f'{v} must contain at least 1 character.'
+        return v
+
+    @validator('iban')
+    def validate_id(cls, v):
+        assert len(v) <= 27, f'maximum length for {v} is 27 characters.'
+        assert len(v) > 0, f'{v} must contain at least 1 character.'
+        return v
+
+    @validator('telefono')
+    def validate_id(cls, v):
+        assert len(v) <= 15, f'maximum length for {v} is 15 characters.'
+        assert len(v) > 0, f'{v} must contain at least 1 character.'
+        return v
+
+    @validator('email')
+    def validate_id(cls, v):
+        assert len(v) <= 90, f'maximum length for {v} is 90 characters.'
+        assert len(v) > 0, f'{v} must contain at least 1 character.'
+        return v
+
+    @validator('pec')
+    def validate_id(cls, v):
+        assert len(v) <= 45, f'maximum length for {v} is 45 characters.'
+        assert len(v) > 0, f'{v} must contain at least 1 character.'
+        return v
+
+    @validator('fax')
+    def validate_id(cls, v):
+        assert len(v) <= 15, f'maximum length for {v} is 15 characters.'
+        assert len(v) > 0, f'{v} must contain at least 1 character.'
+        return v
--- models/company_clientModel.py
+++ models/company_clientModel.py
@@ -1,6 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 class Company_Client(BaseModel):
 
     id_azienda : str
     id_cliente : str
+
+    @validator('id_azienda')
+    def validate_id(cls, v):
+        assert len(v) <= 80, f'maximum length for {v} is 80 characters.'
+        assert len(v) > 0, f'{v} must contain at least 1 character.'
+        return v
+
+    @validator('id_cliente')
+    def validate_id(cls, v):
+        assert len(v) <= 80, f'maximum length for {v} is 80 characters.'
+        assert len(v) > 0, f'{v} must contain at least 1 character.'
+        return v
