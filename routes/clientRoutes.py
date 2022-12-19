eccbeea8a1b3a0a067b2d51a73645b1396584daf
Fabio Cosentino <fabiocosentino7@gmail.com>
2022-12-19 11:26:33 +0100

    refactoring

--- routes/clientRoutes.py
+++ routes/clientRoutes.py
@@ -5,26 +5,26 @@ from models.clientModel import Client
 router = APIRouter(prefix="/clients", tags=["Clients API"])
 
 @router.get("")
-async def get_clients(client_id : str | None = None):
-    if client_id:
-        if Service.get_client_by_id_service(client_id) == None:
-            raise HTTPException(status_code=404, detail=f"Item with key 'id_cliente'='{client_id}' not found")
+async def get_clients(id_cliente : str | None = None):
+    if id_cliente:
+        if Service.get_client_by_id_service(id_cliente) == None:
+            raise HTTPException(status_code=404, detail=f"Item with key 'id_cliente'='{id_cliente}' not found")
     else:
         if Service.get_all_clients_service() == []:
             raise HTTPException(status_code=404, detail=f"No Items Found")
     try:
-        if client_id:
-            return Service.get_client_by_id_service(client_id)
+        if id_cliente:
+            return Service.get_client_by_id_service(id_cliente)
         return Service.get_all_clients_service()
     except Exception as e:
         raise HTTPException(status_code=500, detail=e.msg)
 
 @router.delete("")
-async def delete_client(client_id: str):
-    if Service.get_client_by_id_service(client_id) == None:
-        raise HTTPException(status_code=404, detail=f"Item with key 'id_cliente'='{client_id}' not found")
+async def delete_client(id_cliente: str):
+    if Service.get_client_by_id_service(id_cliente) == None:
+        raise HTTPException(status_code=404, detail=f"Item with key 'id_cliente'='{id_cliente}' not found")
     try:
-        Service.delete_client_by_id_service(client_id)
+        Service.delete_client_by_id_service(id_cliente)
     except Exception as e:
         raise HTTPException(status_code=500, detail=e.msg)
     raise HTTPException(status_code=201, detail="Item Deleted Successfully")
@@ -38,11 +38,11 @@ async def add_client(client : Client):
     raise HTTPException(status_code=201, detail="Item Added Successfully")
 
 @router.put("")
-async def update_client(client_id : str, new_data : Client):
-    if Service.get_client_by_id_service(client_id) == None:
-        raise HTTPException(status_code=404, detail=f"Item with key 'id_cliente'='{client_id}' not found")
+async def update_client(id_cliente : str, new_data : Client):
+    if Service.get_client_by_id_service(id_cliente) == None:
+        raise HTTPException(status_code=404, detail=f"Item with key 'id_cliente'='{id_cliente}' not found")
     try:
-        Service.update_client(client_id, new_data)
+        Service.update_client(id_cliente, new_data)
     except Exception as e:
         raise HTTPException(status_code=500, detail=e.msg)
     raise HTTPException(status_code=201, detail="Item Updated Successfully")
