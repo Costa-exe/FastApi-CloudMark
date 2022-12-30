2c801dab4f861ba53f00623c1bab49e042b792d4
Fabio Cosentino <fabiocosentino7@gmail.com>
2022-12-30 10:50:04 +0100

    added business logic to client dao file

--- dao/clientDao.py
+++ dao/clientDao.py
@@ -4,6 +4,192 @@ from models.clientModel import Client
 class ClientDao:
 
     @classmethod
+    def find_active_by_name(cls, name : str):
+        MySql.open_connection()
+        MySql.query(f"""
+                        SELECT cl.nome, cl.p_iva, cl.email, count(*) as commesse_attive
+                        FROM commessa c, cliente cl
+                        WHERE c.id_cliente = cl.id_cliente
+                        and DATEDIFF(c.data_fine, CURDATE()) > 0
+                        and cl.nome like '{name}%'
+                        GROUP BY cl.p_iva;
+                    """)
+        results = MySql.get_results()
+        MySql.close_connection()
+        return results
+
+    @classmethod
+    def find_inactive_by_name(cls, name : str):
+        MySql.open_connection()
+        MySql.query(f"""
+                        SELECT cl.nome, cl.p_iva, cl.email, if(1 > 0, 0, 0) as commesse_attive
+                        FROM cliente cl
+                        WHERE cl.id_cliente not in (SELECT cl.id_cliente
+                                                    FROM commessa c, cliente cl
+                                                    WHERE c.id_cliente = cl.id_cliente
+                                                    and DATEDIFF(c.data_fine, CURDATE()) > 0
+                                                    GROUP BY cl.id_cliente)
+                        and cl.nome like '{name}%'
+                    """)
+        results = MySql.get_results()
+        MySql.close_connection()
+        return results
+
+    @classmethod
+    def find_active_by_vat(cls, vat : str):
+        MySql.open_connection()
+        MySql.query(f"""
+                        SELECT cl.nome, cl.p_iva, cl.email, count(*) as commesse_attive
+                        FROM commessa c, cliente cl
+                        WHERE c.id_cliente = cl.id_cliente
+                        and DATEDIFF(c.data_fine, CURDATE()) > 0
+                        and cl.p_ive like '{vat}%'
+                        GROUP BY cl.p_iva;
+                    """)
+        results = MySql.get_results()
+        MySql.close_connection()
+        return results
+
+    @classmethod
+    def find_inactive_by_name(cls, vat : str):
+        MySql.open_connection()
+        MySql.query(f"""
+                        SELECT cl.nome, cl.p_iva, cl.email, if(1 > 0, 0, 0) as commesse_attive
+                        FROM cliente cl
+                        WHERE cl.id_cliente not in (SELECT cl.id_cliente
+                                                    FROM commessa c, cliente cl
+                                                    WHERE c.id_cliente = cl.id_cliente
+                                                    and DATEDIFF(c.data_fine, CURDATE()) > 0
+                                                    GROUP BY cl.id_cliente)
+                        and cl.nome like '{vat}%'
+                    """)
+        results = MySql.get_results()
+        MySql.close_connection()
+        return results
+
+    @classmethod
+    def find_active_assignments(cls, vat : str):
+        MySql.open_connection()
+        MySql.query(f"""
+                        SELECT c.id_commessa, c.descrizione
+                        FROM commessa c, cliente cl
+                        WHERE c.id_cliente = cl.id_cliente
+                        and cl.p_iva like '{vat}%'
+                        and DATEDIFF(c.data_fine, CURDATE()) > 0;
+                    """)
+        results = MySql.get_results()
+        MySql.close_connection()
+        return results
+
+    @classmethod
+    def find_all_active(cls):
+        MySql.open_connection()
+        MySql.query(f"""
+                        SELECT cl.nome, cl.cap, cl.email, cl.p_iva, cl.telefono, count(*) as commesse_attive, cl.indirizzo
+                        FROM commessa c, cliente cl
+                        WHERE c.id_cliente = cl.id_cliente
+                        and DATEDIFF(c.data_fine, CURDATE()) > 0
+                        GROUP BY cl.p_iva;
+                    """)
+        results = MySql.get_results()
+        MySql.close_connection()
+        return results
+
+    @classmethod
+    def find_all_inactive(cls):
+        MySql.open_connection()
+        MySql.query(f"""
+                        SELECT cl.nome, cl.cap, cl.email, cl.p_iva, cl.telefono, if(1 > 0, 0, 0) as commesse_attive, cl.indirizzo
+                        FROM cliente cl
+                        WHERE cl.id_cliente not in (SELECT cl.id_cliente
+                                                    FROM commessa c, cliente cl
+                                                    WHERE c.id_cliente = cl.id_cliente
+                                                    and DATEDIFF(c.data_fine, CURDATE()) > 0
+                                                    GROUP BY cl.id_cliente)
+                    """)
+        results = MySql.get_results()
+        MySql.close_connection()
+        return results
+
+    @classmethod
+    def find_active_details_by_name(cls, name : str):
+        MySql.open_connection()
+        MySql.query(f"""
+                        SELECT cl.nome, cl.cap, cl.email, cl.p_iva, cl.telefono, count(*) as commesse_attive, cl.indirizzo
+                        FROM commessa c, cliente cl
+                        WHERE c.id_cliente = cl.id_cliente
+                        and DATEDIFF(c.data_fine, CURDATE()) > 0
+                        and cl.nome like '{name}%'
+                        GROUP BY cl.p_iva;
+                    """)
+        results = MySql.get_results()
+        MySql.close_connection()
+        return results
+
+    @classmethod
+    def find_active_details_by_name(cls, vat : str):
+        MySql.open_connection()
+        MySql.query(f"""
+                        SELECT cl.nome, cl.cap, cl.email, cl.p_iva, cl.telefono, count(*) as commesse_attive, cl.indirizzo
+                        FROM commessa c, cliente cl
+                        WHERE c.id_cliente = cl.id_cliente
+                        and DATEDIFF(c.data_fine, CURDATE()) > 0
+                        and cl.p_iva like '{vat}%'
+                        GROUP BY cl.p_iva;
+                    """)
+        results = MySql.get_results()
+        MySql.close_connection()
+        return results
+
+    @classmethod
+    def find_inactive_details_by_name(cls, name : str):
+        MySql.open_connection()
+        MySql.query(f"""
+                        SELECT cl.nome, cl.cap, cl.email, cl.p_iva, cl.telefono, if(1 > 0, 0, 0) as commesse_attive, cl.indirizzo
+                        FROM cliente cl
+                        WHERE cl.id_cliente not in (select cl.id_cliente
+                                                    from commessa c, cliente cl
+                                                    where c.id_cliente = cl.id_cliente
+                                                    and DATEDIFF(c.data_fine, CURDATE()) > 0
+                                                    group by cl.id_cliente)
+                        and cl.nome like '{name}%'
+                    """)
+        results = MySql.get_results()
+        MySql.close_connection()
+        return results
+
+    @classmethod
+    def find_inactive_details_by_vat(cls, vat : str):
+        MySql.open_connection()
+        MySql.query(f"""
+                        SELECT cl.nome, cl.cap, cl.email, cl.p_iva, cl.telefono, if(1 > 0, 0, 0) as commesse_attive, cl.indirizzo
+                        FROM cliente cl
+                        WHERE cl.id_cliente not in (select cl.id_cliente
+                                                    from commessa c, cliente cl
+                                                    where c.id_cliente = cl.id_cliente
+                                                    and DATEDIFF(c.data_fine, CURDATE()) > 0
+                                                    group by cl.id_cliente)
+                        and cl.nome like '{vat}%'
+                    """)
+        results = MySql.get_results()
+        MySql.close_connection()
+        return results
+
+    @classmethod
+    def find_assignment_details(cls, id : str):
+        MySql.open_connection()
+        MySql.query(f"""
+                        SELECT c.descrizione, d.nome, c.data_inizio, c.data_fine, cd.rate
+                        FROM commessa c, dipendente d, commessa_dipendente cd
+                        WHERE c.id_commessa = cd.id_commessa
+                        and d.id_dipendente = cd.id_dipendente
+                        and c.id_commessa = '{id}'
+                    """)
+        results = MySql.get_result()
+        MySql.close_connection()
+        return results
+
+    @classmethod
     def find_all(cls):
         MySql.open_connection()
         MySql.query("SELECT * FROM cliente")
