6047a37d9909a4d7e7225bfeaf30d04d96528b7b
Fabio Cosentino <fabiocosentino7@gmail.com>
2023-01-12 16:47:00 +0100

    added linux fs uri type

--- utils/db.py
+++ utils/db.py
@@ -3,38 +3,39 @@ import json
 
 from pathlib import Path
 
+
 class MySql:
 
-  @classmethod
-  def open_connection(cls):
-    config = json.loads(Path(r"utils\info.json").read_text())
-    try:
-      cls.conn =  mysql.connect(**config)
-      cls.cursor = cls.conn.cursor(dictionary=True)
-    except mysql.Error as er:
-      print("Connection failed... " + er)
-    finally:
-      return cls.conn.cursor()
-  
-  @classmethod
-  def query(cls, string):
-    cls.cursor.execute(string)
+    @classmethod
+    def open_connection(cls):
+        config = json.loads(Path(r"utils/info.json").read_text())
+        try:
+            cls.conn = mysql.connect(**config)
+            cls.cursor = cls.conn.cursor(dictionary=True)
+        except mysql.Error as er:
+            print("Connection failed... " + er)
+        finally:
+            return cls.conn.cursor()
 
-  @classmethod
-  def get_results(cls):
-    return cls.cursor.fetchall()
-  
-  @classmethod
-  def get_result(cls):
-    return cls.cursor.fetchone()
+    @classmethod
+    def query(cls, string):
+        cls.cursor.execute(string)
 
-  @classmethod
-  def close_connection(cls):
-    cls.cursor.close()
-    cls.conn.close()
+    @classmethod
+    def get_results(cls):
+        return cls.cursor.fetchall()
 
-  @classmethod
-  def close_connection_commit(cls):
-    cls.cursor.close()
-    cls.conn.commit()
-    cls.conn.close()
+    @classmethod
+    def get_result(cls):
+        return cls.cursor.fetchone()
+
+    @classmethod
+    def close_connection(cls):
+        cls.cursor.close()
+        cls.conn.close()
+
+    @classmethod
+    def close_connection_commit(cls):
+        cls.cursor.close()
+        cls.conn.commit()
+        cls.conn.close()
